4abc822bdae5bb692692fd780bced5b0ddf4c4bd
carolinapw <cpicowertag@udesa.edu.ar>
2025-09-15 15:14:00 -0300

    Arreglos en Cargar Mas

--- src/screens/Sections/SeriesPopular.js
+++ src/screens/Sections/SeriesPopular.js
@@ -3,7 +3,7 @@ import ListScreen from "../ListScreen/ListScreen";
 
 class SeriesPopular extends Component {
     render(){ 
-        return <ListScreen {...this.props} type="tv" section="popular" title="Todas las series populares" />; 
+        return <ListScreen type="tv" section="popular" title="Todas las series populares" />; 
     }
 }
 export default SeriesPopular
