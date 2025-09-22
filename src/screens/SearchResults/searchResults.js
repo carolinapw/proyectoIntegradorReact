4704b6a59e59a92467b7374b198703bb7e89b76a
carolinapw <cpicowertag@udesa.edu.ar>
2025-09-22 15:09:50 -0300

    borro error

--- src/screens/SearchResults/searchResults.js
+++ src/screens/SearchResults/searchResults.js
@@ -3,14 +3,13 @@ import React, { Component } from "react";
 const apiKey = "d7dce97c9f45ff25eeb66dc3784d0bca"
 
 class SearchResults extends Component{
-     constructor(props) {
-    super(props);
-    this.state = {
-      query: "", 
-      type: "",
-      data:null, 
-      cargando:false,
-      error: null, 
+    constructor(props) {
+      super(props);
+      this.state = {
+        query: "", 
+        type: "",
+        data:null, 
+        cargando:false,
     };
   }
     
@@ -18,49 +17,32 @@ class SearchResults extends Component{
     let type = this.props.match.params.type
     let query = this.props.match.params.query
   
-      if (type === "movie") {
-
-     fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}&include_adult=false&language=en-US&page=1`)
+    if (type === "movie") {
+      fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}&include_adult=false&language=en-US&page=1`)
         .then(res => res.json())
-      .then(res => {
-        if (res.results && res.results.length > 0) {
+        .then(res => {
           this.setState({
             data: res.results,
             type: type,
             query: query,
             cargando: false
           });
-        } else {
-          this.setState({
-            data: null, 
-            type: type,
-            query: query,
-            cargando: false
-          });
         }
-      })
-        .catch(err => console.error(err));
+        ) 
+        .catch(err => console.log(err));
             }else {
                     fetch(`https://api.themoviedb.org/3/search/tv?api_key=${apiKey}&query=${query}&include_adult=false&language=en-US&page=1`)
         .then(res => res.json())
         .then(res => {
-        if (res.results && res.results.length > 0) {
           this.setState({
             data: res.results,
             type: type,
             query: query,
             cargando: false
           });
-        } else {
-          this.setState({
-            data: null, 
-            type: type,
-            query: query,
-            cargando: false
-          });
         }
-      })
-        .catch(err => console.error(err));
+        )
+        .catch(err => console.log(err));
             }
 
     }
@@ -69,14 +51,8 @@ class SearchResults extends Component{
     if (this.state.cargando) {
       return <p>Cargando resultados....</p>;
     }
-    if (this.state.error){
-        return <p>Error: {this.state.error}</p>
-    }
     if(!this.state.data){
        return <p>No se encontraron resultados</p>
-    }
-    if(this.state.data.length===0){
-        return <p>No se encontraron resultados</p>
     }
 
     return (
